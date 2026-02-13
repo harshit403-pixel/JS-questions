e18d2e0f13c5e8d22f7847de5c5fee4dba6ba5f3
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-02-13 15:16:02 +0530

    Add initial HTML structure and JavaScript functions for array operations

---
+++ script.js
@@ -0,0 +1,78 @@
+// MAIN ARRAY USED IN MOST QUESTION :
+
+let arr = [1,2,3,4,55,60]
+
+
+
+//1. Write a function that uses `forEach()` to calculate the total sum of an array. 
+
+function totalSum(a){
+    let total = 0;
+    a.forEach((element) => {
+        total = total+element
+    });
+    return total;
+}
+console.log("total sum of array :" ,totalSum(arr));
+
+// 2. Write a function that uses `map()` to return a new array where each number is squared.
+
+function squared(b){
+
+   let bSq =  b.map((elem)=>{
+        return elem*elem;
+    })
+    return bSq
+
+}
+console.log("sqaure of array :" ,squared(arr));
+
+// 3. Write a function that uses `filter()` to return only numbers greater than 50.
+
+function greaterthan50(c){
+ let cG50 =    c.filter((x)=>{
+        return x>50
+    })
+
+    return cG50;
+}
+console.log("greater than 50 :" ,greaterthan50(arr));
+
+// 4. Write a function that checks whether two values are strictly equal using `===`.
+
+let a = 50
+let b = '50'
+
+function strictCheck(a, b){
+    if(a===b){
+        return true
+    }
+    else{
+        return false
+    }
+
+}
+console.log("check by === :",strictCheck(a,b));
+
+// 5. Write a function that demonstrates array mutability by modifying the original array using `push()`.
+ 
+console.log("before push :" ,arr);
+
+function mutability(arr){
+    arr.push(100)
+    console.log("after push :" , arr);
+}
+mutability(arr)
+
+arr.pop()
+console.log("used pop :",arr);
+// so that remaining questions not affected by push
+
+
+// 6. Write a function that removes the last element immutably (without modifying original array).
+
+function immutableRemove(arr){
+    console.log("used slice :",arr.slice(0, length-2));    
+}
+immutableRemove(arr)
+
