64df85f0653b13e52a469b82cba1f41fc64f882e
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-02-19 02:17:44 +0530

    Remove unused box elements and styles; retain random number generation and jackpot display functionality

--- DOM Tasks 2/Task 7/script.js
+++ DOM Tasks 2/Task 7/script.js
@@ -1,73 +1,17 @@
 
-// let body = document.querySelector("body")
+let body = document.querySelector("body")
 
-// let box1 = document.createElement("div")
-// let box2 = document.createElement("div")
-// let box3 = document.createElement("div")
-// let box4 = document.createElement("div")
-// let box5 = document.createElement("div")
-
-// box1.setAttribute("data-id","1")
-// box2.setAttribute("data-id","2")
-// box3.setAttribute("data-id","3")
-// box4.setAttribute("data-id","4")
-// box5.setAttribute("data-id","5")
-
-// box1.classList.add("box")
-// box2.classList.add("box")
-// box3.classList.add("box")
-// box4.classList.add("box")
-// box5.classList.add("box")
-
-// body.appendChild(box1)
-// body.appendChild(box2)
-// body.appendChild(box3)
-// body.appendChild(box4)
-// body.appendChild(box5)
-
-
-
-// box1.addEventListener("click",()=>{
-//    box1.style.display = "none"
-//    console.log(box1.getAttribute("data-id"));
-// })
-// box2.addEventListener("click",()=>{
-//    box2.style.display = "none"
-//    console.log(box2.getAttribute("data-id"));
-// })
-// box3.addEventListener("click",()=>{
-//    box3.style.display = "none"
-//    console.log(box3.getAttribute("data-id"));
-// })
-// box4.addEventListener("click",()=>{
-//    box4.style.display = "none"
-//    console.log(box4.getAttribute("data-id"));
-// })
-// box5.addEventListener("click",()=>{
-//    box5.style.display = "none"
-//    console.log(box5.getAttribute("data-id"));
-// })
-
-
-
-
-let body = document.querySelector("body");
-
-for (let i = 1; i <= 5; i++) {
-
-   let box = document.createElement("div");
-
-   box.classList.add("box");
-   box.setAttribute("data-id", i)
-
-   body.appendChild(box);
-
-   box.addEventListener("click", () => {
-      box.style.display = "none";
-      console.log(box.getAttribute("data-id"));
-   });
- 
-}
-
-
+  let a =  Math.floor(Math.random()*10)
+  let b =  Math.floor(Math.random()*10)
+  let c =  Math.floor(Math.random()*10)
+  console.log(a,b,c);
+  
+  if( a == b && b==c){
+   let h1 = document.createElement("h1")
+   h1.innerHTML = "JACKPOT"
+   h1.style.color = "white"
+   h1.style.fontSize = "200px"
+   body.appendChild(h1)
+  }
+  
 
